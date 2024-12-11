4ff1d5a9660d6d986a058d208b5bba5f283864bb
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 13:27:37 +0800

    href and cursor pointer on images

--- src/app/api/avatar/display/page.js
+++ src/app/api/avatar/display/page.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Display() {
 const router = useRouter();

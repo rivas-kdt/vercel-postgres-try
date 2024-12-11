3be2a827f454fa12158e320b6ebfd3af3c2088f4
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 14:29:31 +0800

    href and cursor pointer on images

--- src/app/tag-image/[id]/page.js
+++ src/app/tag-image/[id]/page.js
@@ -1,20 +1,17 @@
-
-export async function fetchImage(id) {
-    try{
-        const response = await axios.get(`/api/avatar/get-image?id=${id}`)
-        return response.data;
-    }catch(err){
-        console.log(err)
-    }
+async function fetchImage(id) {
+  try {
+    const response = await axios.get(`/api/avatar/get-image?id=${id}`);
+    console.log(response)
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export default async function TagImage({params}) {
-    const { id } = await params
-    const data = fetchImage(id)
-    console.log(data)
-    return (
-        <div>
-            {id}
-        </div>
-    )
+export default async function TagImage({ params }) {
+  const { id } = await params;
+  const data = fetchImage(id);
+  console.log(id);
+  console.log(data);
+  return <div>{id}</div>;
 }

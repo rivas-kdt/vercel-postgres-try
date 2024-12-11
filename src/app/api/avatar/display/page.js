5d8b7bbbe26555defc40aa0bda3e0c4f00b8a9e8
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 12:26:14 +0800

    masonry-grid try

--- src/app/api/avatar/display/page.js
+++ src/app/api/avatar/display/page.js
@@ -22,17 +22,19 @@ export default function Display() {
   console.log(error);
   return (
     <div className="flex items-center justify-center w-screen h-screen gap-2">
-      <div className="flex flex-wrap gap-2">
-        {data.map((img) => (
-          <div key={img.id}>
-            <Image
-              src={img.url}
-              sizes="250px"
-              loading="lazy"
-              className="w-full h-auto "
-            ></Image>
-          </div>
-        ))}
+      <div className="w-full max-w-7xl">
+        <div className="gap-4 columns-1 sm:columns-2 md:columns-3">
+          {data.map((img) => (
+            <div key={img.id} className="relative mb-4 break-inside-avoid">
+              <Image
+                src={img.url}
+                sizes="250px"
+                loading="lazy"
+                className="w-full h-auto "
+              ></Image>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

caf6d9999c214a28f2ef10d46029c20c85bef8c6
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 09:55:20 +0800

    fix rendering of image

--- src/app/api/avatar/display/page.js
+++ src/app/api/avatar/display/page.js
@@ -1,35 +1,33 @@
-"use client"
+"use client";
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Display() {
-    const [data,setData]=useState([]);
-    const [error,setError]=useState()
-    useEffect(() => {
-        // Fetch data when the component mounts
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(
-              "/api/avatar/save-image"
-            );
-            setData(response.data);
-          } catch (err) {
-            setError(err.message);
-          }
-        };
-        fetchData();
-      }, []);
-      console.log(data)
-      console.log(error)
+  const [data, setData] = useState([]);
+  const [error, setError] = useState();
+  useEffect(() => {
+    // Fetch data when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/avatar/save-image");
+        setData(response.data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data);
+  console.log(error);
   return (
     <div>
-        {data.map((img)=>{
-            <div key={img.id} className=" w-4/5 h-[25%] bg-yellow-500 relative">
-                <p>Image</p>
-                <Image src={img.url} fill className="object-contain "></Image>
-            </div>
-        })}
+      {data.map((img) => (
+        <div key={img.id} className=" w-4/5 h-[25%] bg-yellow-500 relative">
+          <p>Image</p>
+          <Image src={img.url} fill className="object-contain "></Image>
+        </div>
+      ))}
     </div>
-  )
+  );
 }

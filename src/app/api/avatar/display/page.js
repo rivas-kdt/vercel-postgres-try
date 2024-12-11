6092c62998c02de470ef843ef0360736ff0216d1
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 10:26:02 +0800

    style the display page

--- src/app/api/avatar/display/page.js
+++ src/app/api/avatar/display/page.js
@@ -24,7 +24,7 @@ export default function Display() {
     <div className="flex items-center justify-center w-screen h-screen">
       {data.map((img) => (
         <div key={img.id} className="bg-blue-500">
-          <Image src={img.url} width="0" height="0" className="w-[450px] h-auto"></Image>
+          <Image src={img.url} width="0" height="0" loading="lazy" className="w-[450px] h-auto"></Image>
         </div>
       ))}
     </div>

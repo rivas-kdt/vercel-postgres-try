3f080cb878501ed06c516b4448320170aef780f2
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-12-11 14:00:09 +0800

    href and cursor pointer on images

--- src/app/api/avatar/get_image/route.js
+++ src/app/api/avatar/get_image/route.js
@@ -6,7 +6,7 @@ export async function GET(request) {
   try {
     const sql = neon(process.env.DATABASE_URL);
 
-    const inc = await sql("SELECT * FROM images WHERE id = ?", [id]);
+    const inc = await sql("SELECT * FROM images WHERE id = ($1)", [id]);
 
     return new Response(JSON.stringify(inc), {
       status: 200,
